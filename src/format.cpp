71953950c388b3d48928cb4fc622861ec8af5d87
Karlo Sebalj <karlo.sebalj@gmail.com>
2020-04-01 19:49:33 +0200

    Format time working

--- src/format.cpp
+++ src/format.cpp
@@ -9,11 +9,41 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
+
     std::string totalTime;
+    std::string strHour, strMinute, strSecond;
     long minutes, hours;
+
     minutes = seconds / 60;
+    seconds = seconds % 60;
+
     hours = minutes / 60;
-    if (hours < 1) { hours = 00; }
-    totalTime = std::to_string(hours) + ":" + std::to_string(minutes % 60) + ":" + std::to_string(seconds % 60);
-    return totalTime; 
+    minutes= minutes % 60;
+
+    strHour = std::to_string(hours);
+    strMinute = std::to_string(minutes);
+    strSecond = std::to_string(seconds);
+
+    if (hours < 10 ) {
+        strHour = "0" + strHour;
     }
+
+    if (minutes < 10) {
+        strMinute = "0" + strMinute;
+    }
+    
+    if (seconds < 10) {
+        strSecond = "0" + strSecond;
+    }
+
+    if (strSecond == "60") {
+        strSecond = "00";
+    }
+
+    if (strMinute == "60") {
+        strMinute = "00";
+    }
+
+    totalTime =  strHour + ":" + strMinute + ":" + strSecond;
+    return totalTime; 
+}
